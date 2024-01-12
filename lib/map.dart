62cfe5a8db02de27d609b880c475daf1d2a010f9
wiyco <72733890+wiyco@users.noreply.github.com>
2024-01-12 23:53:44 +0900

    Update map.dart

--- lib/map.dart
+++ lib/map.dart
@@ -1,19 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
-class MapboxPage extends StatelessWidget {
+class MapboxPage extends StatefulWidget {
   const MapboxPage({super.key, required this.mapboxPublicToken});
 
   final String mapboxPublicToken;
 
   @override
+  State<MapboxPage> createState() => _MapboxPageState();
+}
+
+class _MapboxPageState extends State<MapboxPage> {
+  MapboxMap? mapboxMap;
+
+  _onMapCreated(MapboxMap mapboxMap) {
+    this.mapboxMap = mapboxMap;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: const Text('Mapbox'),
-        ),
-        body: MapWidget(
-            resourceOptions: ResourceOptions(accessToken: mapboxPublicToken)));
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Mapbox'),
+      ),
+      body: MapWidget(
+        key: const ValueKey("mapWidget"),
+        resourceOptions: ResourceOptions(accessToken: widget.mapboxPublicToken),
+        onMapCreated: _onMapCreated,
+      ),
+    );
   }
 }
