562d272c6e7d0dd1f10505c4ff7462afa8d6a0be
wiyco <72733890+wiyco@users.noreply.github.com>
2024-01-14 15:45:50 +0900

    [Map] Code cleanup

--- lib/map.dart
+++ lib/map.dart
@@ -3,7 +3,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:geolocator/geolocator.dart' as g;
+import 'package:geolocator/geolocator.dart' as gl;
 
 class MapboxPage extends StatefulWidget {
   const MapboxPage({super.key, required this.mapboxPublicToken});
@@ -15,12 +15,12 @@ class MapboxPage extends StatefulWidget {
 }
 
 class _MapboxPageState extends State<MapboxPage> {
-  g.Position? currentPosition;
   late MapboxMap _mapboxMap;
-  late StreamSubscription<g.Position> _currentPositionStream;
+  late StreamSubscription<gl.Position> _currentPositionStream;
+  gl.Position? _currentPosition;
 
-  final _locationSettings = const g.LocationSettings(
-    accuracy: g.LocationAccuracy.high,
+  final _locationSettings = const gl.LocationSettings(
+    accuracy: gl.LocationAccuracy.best,
     distanceFilter: 10,
   );
 
@@ -33,34 +33,45 @@ class _MapboxPageState extends State<MapboxPage> {
   void initState() {
     super.initState();
 
-    _requestPermissions();
+    _currentPositionStream =
+        gl.Geolocator.getPositionStream(locationSettings: _locationSettings)
+            .listen((gl.Position? position) {
+      if (position != null) {
+        _currentPosition = position;
+        _updateCameraPosition(position);
+      }
+    });
   }
 
   @override
   void dispose() {
+    _currentPositionStream.cancel();
     super.dispose();
   }
 
   void _onMapCreated(MapboxMap mapboxMap) {
     _mapboxMap = mapboxMap;
+    _initCurrentLocation();
     _showCurrentLocation();
-    _currentPositionStream =
-        g.Geolocator.getPositionStream(locationSettings: _locationSettings)
-            .listen((g.Position? position) {
-      currentPosition = position;
-      _updateCameraPosition();
-    });
   }
 
-  void _requestPermissions() async {
-    PermissionStatus permissionLocation = await Permission.location.status;
-    if (permissionLocation.isDenied || permissionLocation.isPermanentlyDenied) {
-      await Permission.location.request();
-    }
+  void _initCurrentLocation() async {
+    await Permission.location.request();
+    _currentPosition = await gl.Geolocator.getCurrentPosition(
+        desiredAccuracy: _locationSettings.accuracy);
+    await _mapboxMap.setCamera(CameraOptions(
+      center: Point(
+        coordinates: Position(
+          _currentPosition!.longitude,
+          _currentPosition!.latitude,
+        ),
+      ).toJson(),
+      zoom: 15,
+    ));
   }
 
-  void _showCurrentLocation() async {
-    await _mapboxMap.location.updateSettings(LocationComponentSettings(
+  void _showCurrentLocation() {
+    _mapboxMap.location.updateSettings(LocationComponentSettings(
       enabled: true,
       puckBearingEnabled: true,
       pulsingEnabled: true,
@@ -68,23 +79,21 @@ class _MapboxPageState extends State<MapboxPage> {
     ));
   }
 
-  void _updateCameraPosition() {
-    _currentPositionStream.onData((g.Position position) {
-      _mapboxMap.easeTo(
-          CameraOptions(
-            center: Point(
-              coordinates: Position(
-                position.longitude,
-                position.latitude,
-              ),
-            ).toJson(),
-            zoom: 13.5,
-          ),
-          MapAnimationOptions(
-            duration: 1000,
-            startDelay: 0,
-          ));
-    });
+  void _updateCameraPosition(gl.Position position) {
+    _mapboxMap.easeTo(
+        CameraOptions(
+          center: Point(
+            coordinates: Position(
+              position.longitude,
+              position.latitude,
+            ),
+          ).toJson(),
+          zoom: 15,
+        ),
+        MapAnimationOptions(
+          duration: 500,
+          startDelay: 0,
+        ));
   }
 
   @override
@@ -99,8 +108,6 @@ class _MapboxPageState extends State<MapboxPage> {
         resourceOptions: ResourceOptions(accessToken: widget.mapboxPublicToken),
         cameraOptions: _cameraOptions,
         onMapCreated: _onMapCreated,
-        onCameraChangeListener: (cameraChangedEventData) =>
-            _updateCameraPosition(),
       ),
     );
   }
