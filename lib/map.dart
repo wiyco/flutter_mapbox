629a48a9113e42374e6bfbcfeeec509c5e66f043
wiyco <72733890+wiyco@users.noreply.github.com>
2024-01-19 10:11:51 +0900

    [Map] Add comments

--- lib/map.dart
+++ lib/map.dart
@@ -19,11 +19,13 @@ class _MapboxPageState extends ConsumerState<MapboxPage> {
   late StreamSubscription<gl.Position> _currentPositionStream;
   static gl.Position? currentPosition;
 
+  // Default geolocation settings
   final _locationSettings = const gl.LocationSettings(
     accuracy: gl.LocationAccuracy.best,
     distanceFilter: 10,
   );
 
+  // Default map camera settings
   final _cameraOptions = CameraOptions(
     center: Point(coordinates: Position(0, 0)).toJson(),
     zoom: 1,
@@ -32,7 +34,7 @@ class _MapboxPageState extends ConsumerState<MapboxPage> {
   @override
   void initState() {
     super.initState();
-
+    // https://pub.dev/packages/geolocator#listen-to-location-updates
     _currentPositionStream =
         gl.Geolocator.getPositionStream(locationSettings: _locationSettings)
             .listen((gl.Position? position) {
@@ -47,6 +49,8 @@ class _MapboxPageState extends ConsumerState<MapboxPage> {
     super.dispose();
   }
 
+  // After the map is created, set the camera position to the current location
+  // and show the current location on the map.
   void _onMapCreated(MapboxMap mapboxMap) {
     _mapboxMap = mapboxMap;
     _initCurrentLocation();
@@ -54,9 +58,12 @@ class _MapboxPageState extends ConsumerState<MapboxPage> {
   }
 
   void _initCurrentLocation() async {
+    // https://pub.dev/packages/permission_handler
     await Permission.location.request();
+    // Get the current location
     currentPosition = await gl.Geolocator.getCurrentPosition(
         desiredAccuracy: gl.LocationAccuracy.high);
+    // Set the camera position to the current location
     await _mapboxMap.setCamera(CameraOptions(
       center: Point(
         coordinates: Position(
@@ -69,6 +76,7 @@ class _MapboxPageState extends ConsumerState<MapboxPage> {
   }
 
   void _showCurrentLocation() {
+    // https://pub.dev/packages/mapbox_maps_flutter
     _mapboxMap.location.updateSettings(LocationComponentSettings(
       enabled: true,
       puckBearingEnabled: true,
@@ -78,6 +86,7 @@ class _MapboxPageState extends ConsumerState<MapboxPage> {
   }
 
   void _updateCameraPosition(gl.Position position) {
+    // https://pub.dev/packages/mapbox_maps_flutter#camera-animations
     _mapboxMap.easeTo(
         CameraOptions(
           center: Point(
